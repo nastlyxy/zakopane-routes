072ba048d7339b81734a3c1f0ea9fa8d253b2be0
Anastasiya Haiduk <anastasiagajduk24@gmail.com>
2026-04-21 23:37:54 +0200

    added home page content and some css styles

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,25 @@
-export default function HomePage(){
-    return(
-        <div className="home-page-container">
-            <h1>Best routes in Zakopane</h1>
-        </div>
-    )
+import { Link } from "react-router-dom";
+import { ThemeContext } from "../ThemeContext";
+import { useContext } from "react";
+
+import HeroImage from "../assets/images/tatry.jpg";
+
+export default function HomePage() {
+  const isDarkMode = useContext(ThemeContext);
+
+  return (
+    <div className={`homepage-container ${isDarkMode && "dark"}`}>
+      <div className="hero-content">
+        <h1 className="homepage-title">Discover the Polish Tatras</h1>
+        <p className="homepage-description">
+          Your personal guide to the Polish Tatras. Choose routes by difficulty
+          level, save your favorite trails, and find the perfect company for
+          mountain adventures.
+        </p>
+        <Link className="routes-link" to={"/routes"}>
+          Explore Trails
+        </Link>
+      </div>
+    </div>
+  );
 }
